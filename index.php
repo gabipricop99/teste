0701095466d5e5a1ff16c53b2ebd5f9617ea55e8
gabipricop99 <62254381+gabipricop99@users.noreply.github.com>
2023-09-10 01:04:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <style>
         body {
             font-family: Arial, sans-serif;
-            background-color: #000; /* Schimbăm culoarea de fundal la negru pentru efectul Matrix */
+            background-image: url('https://cdn-media-1.freecodecamp.org/images/1*c4DFt6p5PDhOxHTx2p_lag.gif'); /* URL-ul imaginii de fundal */
+            background-size: cover; /* Faceți imaginea să acopere întregul fundal */
             text-align: center;
             margin: 0;
             padding: 0;
@@ -17,19 +18,19 @@
         .container {
             max-width: 600px;
             margin: 100px auto;
-            background-color: #000; /* Păstrăm fundalul containerului la negru */
+            background-color: rgba(255, 255, 255, 0.8); /* Adăugăm un fundal semi-transparent pentru a face textul mai ușor de citit peste imagine */
             padding: 20px;
             border-radius: 10px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
         }
 
         h1 {
-            color: #00FF00; /* Culoare verde pentru textul din container */
+            color: #333;
         }
 
         p {
             font-size: 18px;
-            color: #00FF00; /* Culoare verde pentru textul din container */
+            color: #555;
         }
 
         a {
@@ -43,23 +44,26 @@
             color: #00568b;
         }
 
-        /* Console Styling */
-        .console-container {
-            display: flex;
-            justify-content: center;
+        /* Stilurile pentru afișarea textului în stil "Matrix" */
+        .matrix-text {
+            background-color: rgba(0, 0, 0, 0.8); /* Fundal semi-transparent pentru textul "Matrix" */
+            color: #fff; /* Text alb */
+            font-family: monospace;
+            white-space: pre;
+            overflow: auto;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
         }
 
-        .console {
-            background-color: #000; /* Schimbăm culoarea fundalului consolei la negru pentru efectul Matrix */
-            color: #00FF00; /* Culoarea textului consolei va fi verde */
-            padding: 10px;
-            border-radius: 5px;
-            text-align: left;
-            font-family: monospace;
-            width: 80%;
-            white-space: nowrap;
-            overflow: hidden;
-            border: 1px solid #00FF00; /* Bordură verde pentru consolă */
+        /* Definim culoarea textului pentru clasa .matrix-text-alb */
+        .matrix-text-alb {
+            color: #fff; /* Setăm culoarea textului la alb */
+        }
+
+        /* Definim culoarea textului pentru clasa .text-rosu */
+        .text-rosu {
+            color: red; /* Setăm culoarea textului la roșu */
         }
     </style>
 </head>
@@ -70,82 +74,81 @@
         <p>For details, contact <a href="https://t.me/georgedev/" target="_blank">georgedev</a>.</p>
     </div>
 
-    <!-- Adăugăm fundalul cu conținutul dorit -->
-    <div class="matrix-background">
-        <!-- Puteți adăuga conținutul dorit aici -->
-        <!-- Am adăugat conținutul specific pe care l-ați furnizat -->
-        1	altenabet.com 	altenabet	Passcraked
-2	betsler.com 	betsler	Passcraked
-3	bettingexchangescript.com 	bettingexchangxx	Passcraked
-4	bettingexchangesoftware.com 	bettingexchanges	Passcraked
-5	betting-script.com 	bettingscript	Passcraked
-6	casinohades.com 	casinohades2	Passcraked
-7	decasinos.io 	decasinos	Passcraked
-8	cryptocasinoscripts.com 	cryptocasinoscri	Passcraked
-9	nulledcasino.com 	nulledcasino	Passcraked
-10	prowagersystem.com 	prowagersystem	Passcraked
-11	qaiscasino.com 	qaiscasino	Passcraked
-12	whitelabelcasino.io 	whitelabelcasino	Passcraked
-13	goldsvetscript.com 	goldsvetscript34	Passcraked
-14	demo9.goldsvetscript.com 	demo9goldsvetscr	Passcraked
-15	demo6.goldsvetscript.com 	demo6goldsvetscr	Passcraked
-16	demo2.goldsvetscript.com 	demo2goldsvetscr	Passcraked
-17	xwin.goldsvetscript.com 	xwingoldsvetscri	Passcraked
-18	golden444.altenabet.com 	golden444altenab	Passcraked
-19	1bet365.altenabet.com 	bet365altenabet	Passcraked
-20	onix.altenabet.com 	onixaltenabet	Passcraked
-21	neonwin.altenabet.com 	neonwinaltenabet	Passcraked
-22	betbrain.altenabet.com 	betbrainaltenabe	Passcraked
-23	88bet.altenabet.com 	betaltenabet88	Passcraked
-24	win8x.altenabet.com 	win8xaltenabet	Passcraked
-25	demo4.goldsvetscript.com 	demo4goldsvetscr	Passcraked
-26	alphabet.altenabet.com 	alphabetaltenabe	Passcraked
-27	betking.altenabet.com 	betkingalten3bet	Passcraked
-28	betwin.altenabet.com 	betwina33enabet	Passcraked
-29	bethades.altenabet.com 	bethadesaltena33	Passcraked
-30	xybets88.altenabet.com 	xybets88altenabf	Passcraked
-31	casino365.altenabet.com 	casino365alt2233	Passcraked
-32	sport365.altenabet.com 	sport365alte4abe	Passcraked
-33	slotvegas.altenabet.com 	slotvegasal22nab	Passcraked
-34	redbet.altenabet.com 	redb333altenabet	Passcraked
-35	pixbet.altenabet.com 	pixbewwltenabet	Passcraked
-36	nairabet.altenabet.com 	nairabetal65nabe	Passcraked
-37	ligastavo.altenabet.com 	ligastavoalednab	Passcraked
-38	honeybet.altenabet.com 	honeybetal33nabe	Passcraked
-39	goal365.altenabet.com 	goal365al43nabet	Passcraked
-40	freddybet.altenabet.com 	freddyb22altenab	Passcraked
-41	eurobet.altenabet.com 	eurobet22tenabet	Passcraked
-42	boomcasino.altenabet.com 	boomcasino22tena	Passcraked
-43	betwizard.altenabet.com 	betwidardaltenab	Passcraked
-44	betlock365.com 	betlock365	Passcraked
-45	bitcasino.altenabet.com 	bitcasinoaltenab	Passcraked
-46	demo5.goldsvetscript.com 	demo5g33dsvetscr	Passcraked
-47	casinosoftware.io 	casinosoftware3	Passcraked
-48	apostasistema.com 	apostasistema	Passcraked
-49	play666.altenabet.com 	play666altenabet	Passcraked
-50	goldsvetcasino.com 	goldsvetcasino	Passcraked
-51	onlinescammer.com 	onlinescammer	Passcraked
-52	demo7.goldsvetscript.com 	demogoldsvetscr2	Passcraked
-53	demo.goldsvetscript.com 	demogoldsvetscru	Passcraked
-54	mariobet.altenabet.com 	mariobetaltenabe	Passcraked
-55	spacebet.altenabet.com 	betmartaltenabet	Passcraked
-56	latinbets.altenabet.com 	latinbetsaltenab	Passcraked
-57	betmart.altenabet.com 	betmar22ltenabet	Passcraked
-58	bit2moon.altenabet.com 	bit2moonaltenabe	Passcraked
-59	mywin777.altenabet.com 	mywin777altenabe	Passcraked
-60	api.lokebet.com 	apilokebet	Passcraked
-61	unicornstore.se 	unicornstore	Passcraked
-62	coinpoker.altenabet.com 	coinpokeraltenab	Passcraked
-63	canada.goldsvetcasino.com 	canadagoldsvetca	Passcraked
-64	poppycasino.com 	poppycasino	Passcraked
-65	betfurycasino.altenabet.com 	betfurycasino	Passcraked
-66	play4x.altenabet.com 	play4xaltenabet	Passcraked
-67	betplay.goldsvetscript.com 	betplaygoldsvets	Passcraked
-68	hotslots.goldsvetscript.com 	hotslotsgoldsvet	Passcraked
-69	ninjacasino.altenabet.com 	ninjacasinoalten	Passcraked
-70	dynobets.com 	dynobets	Passcraked
-71	betexchange.altenabet.com 	betexchangealten	Passcraked
+    <!-- Afișăm textul în stil "Matrix" într-un div cu clasa "matrix-text matrix-text-alb" -->
+    <div class="matrix-text matrix-text-alb">
+       1	altenabet.com 	altenabet	SAME SCAMMER
+2	betsler.com 	betsler	SAME SCAMMER
+3	bettingexchangescript.com 	bettingexchangxx	SAME SCAMMER
+4	bettingexchangesoftware.com 	bettingexchanges	SAME SCAMMER
+5	betting-script.com 	bettingscript	SAME SCAMMER
+6	casinohades.com 	casinohades2	SAME SCAMMER
+7	decasinos.io 	decasinos	SAME SCAMMER
+8	cryptocasinoscripts.com 	cryptocasinoscri	SAME SCAMMER
+9	nulledcasino.com 	nulledcasino	SAME SCAMMER
+10	prowagersystem.com 	prowagersystem	SAME SCAMMER
+11	qaiscasino.com 	qaiscasino	SAME SCAMMER
+12	whitelabelcasino.io 	whitelabelcasino	SAME SCAMMER
+13	goldsvetscript.com 	goldsvetscript34	SAME SCAMMER
+14	demo9.goldsvetscript.com 	demo9goldsvetscr	SAME SCAMMER
+15	demo6.goldsvetscript.com 	demo6goldsvetscr	SAME SCAMMER
+16	demo2.goldsvetscript.com 	demo2goldsvetscr	SAME SCAMMER
+17	xwin.goldsvetscript.com 	xwingoldsvetscri	SAME SCAMMER
+18	golden444.altenabet.com 	golden444altenab	SAME SCAMMER
+19	1bet365.altenabet.com 	bet365altenabet	SAME SCAMMER
+20	onix.altenabet.com 	onixaltenabet	SAME SCAMMER
+21	neonwin.altenabet.com 	neonwinaltenabet	SAME SCAMMER
+22	betbrain.altenabet.com 	betbrainaltenabe	SAME SCAMMER
+23	88bet.altenabet.com 	betaltenabet88	SAME SCAMMER
+24	win8x.altenabet.com 	win8xaltenabet	SAME SCAMMER
+25	demo4.goldsvetscript.com 	demo4goldsvetscr	SAME SCAMMER
+26	alphabet.altenabet.com 	alphabetaltenabe	SAME SCAMMER
+27	betking.altenabet.com 	betkingalten3bet	SAME SCAMMER
+28	betwin.altenabet.com 	betwina33enabet	SAME SCAMMER
+29	bethades.altenabet.com 	bethadesaltena33	SAME SCAMMER
+30	xybets88.altenabet.com 	xybets88altenabf	SAME SCAMMER
+31	casino365.altenabet.com 	casino365alt2233	SAME SCAMMER
+32	sport365.altenabet.com 	sport365alte4abe	SAME SCAMMER
+33	slotvegas.altenabet.com 	slotvegasal22nab	SAME SCAMMER
+34	redbet.altenabet.com 	redb333altenabet	SAME SCAMMER
+35	pixbet.altenabet.com 	pixbewwltenabet	SAME SCAMMER
+36	nairabet.altenabet.com 	nairabetal65nabe	SAME SCAMMER
+37	ligastavo.altenabet.com 	ligastavoalednab	SAME SCAMMER
+38	honeybet.altenabet.com 	honeybetal33nabe	SAME SCAMMER
+39	goal365.altenabet.com 	goal365al43nabet	SAME SCAMMER
+40	freddybet.altenabet.com 	freddyb22altenab	SAME SCAMMER
+41	eurobet.altenabet.com 	eurobet22tenabet	SAME SCAMMER
+42	boomcasino.altenabet.com 	boomcasino22tena	SAME SCAMMER
+43	betwizard.altenabet.com 	betwidardaltenab	SAME SCAMMER
+44	betlock365.com 	betlock365	SAME SCAMMER
+45	bitcasino.altenabet.com 	bitcasinoaltenab	SAME SCAMMER
+46	demo5.goldsvetscript.com 	demo5g33dsvetscr	SAME SCAMMER
+47	casinosoftware.io 	casinosoftware3	SAME SCAMMER
+48	apostasistema.com 	apostasistema	SAME SCAMMER
+49	play666.altenabet.com 	play666altenabet	SAME SCAMMER
+50	goldsvetcasino.com 	goldsvetcasino	SAME SCAMMER
+51	onlinescammer.com 	onlinescammer	SAME SCAMMER
+52	demo7.goldsvetscript.com 	demogoldsvetscr2	SAME SCAMMER
+53	demo.goldsvetscript.com 	demogoldsvetscru	SAME SCAMMER
+54	mariobet.altenabet.com 	mariobetaltenabe	SAME SCAMMER
+55	spacebet.altenabet.com 	betmartaltenabet	SAME SCAMMER
+56	latinbets.altenabet.com 	latinbetsaltenab	SAME SCAMMER
+57	betmart.altenabet.com 	betmar22ltenabet	SAME SCAMMER
+58	bit2moon.altenabet.com 	bit2moonaltenabe	SAME SCAMMER
+59	mywin777.altenabet.com 	mywin777altenabe	SAME SCAMMER
+60	api.lokebet.com 	apilokebet	SAME SCAMMER
+61	unicornstore.se 	unicornstore	SAME SCAMMER
+62	coinpoker.altenabet.com 	coinpokeraltenab	SAME SCAMMER
+63	canada.goldsvetcasino.com 	canadagoldsvetca	SAME SCAMMER
+64	poppycasino.com 	poppycasino	SAME SCAMMER
+65	betfurycasino.altenabet.com 	betfurycasino	SAME SCAMMER
+66	play4x.altenabet.com 	play4xaltenabet	SAME SCAMMER
+67	betplay.goldsvetscript.com 	betplaygoldsvets	SAME SCAMMER
+68	hotslots.goldsvetscript.com 	hotslotsgoldsvet	SAME SCAMMER
+69	ninjacasino.altenabet.com 	ninjacasinoalten	SAME SCAMMER
+70	dynobets.com 	dynobets	SAME SCAMMER
+71	betexchange.altenabet.com 	betexchangealten	SAME SCAMMER
 
+        <!-- Adăugați restul textului aici -->
     </div>
 
     <!-- Console Section -->
@@ -157,7 +160,7 @@
     <script>
         // JavaScript for dynamic background change
         const body = document.body;
-        const colors = ['#000', '#000', '#000', '#000', '#000']; /* Păstrăm culoarea de fundal la negru pentru efectul Matrix */
+        const colors = ['rgba(0, 0, 0, 0.8)', 'rgba(0, 0, 0, 0.8)', 'rgba(0, 0, 0, 0.8)', 'rgba(0, 0, 0, 0.8)', 'rgba(0, 0, 0, 0.8)'];
 
         let currentColorIndex = 0;
 
