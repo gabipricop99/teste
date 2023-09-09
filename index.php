b73f6fdf8cda9d2a74ec8c3fea83a0dc6ff50944
gabipricop99 <62254381+gabipricop99@users.noreply.github.com>
2023-09-09 23:53:58 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <style>
         body {
             font-family: Arial, sans-serif;
-            background-color: #f0f0f0;
+            background-color: #000; /* Schimbăm culoarea de fundal la negru pentru efectul Matrix */
             text-align: center;
             margin: 0;
             padding: 0;
@@ -17,19 +17,19 @@
         .container {
             max-width: 600px;
             margin: 100px auto;
-            background-color: #ffffff;
+            background-color: #000; /* Păstrăm fundalul containerului la negru */
             padding: 20px;
             border-radius: 10px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
         }
 
         h1 {
-            color: #333;
+            color: #00FF00; /* Culoare verde pentru textul din container */
         }
 
         p {
             font-size: 18px;
-            color: #555;
+            color: #00FF00; /* Culoare verde pentru textul din container */
         }
 
         a {
@@ -50,8 +50,8 @@
         }
 
         .console {
-            background-color: #333;
-            color: #fff;
+            background-color: #000; /* Schimbăm culoarea fundalului consolei la negru pentru efectul Matrix */
+            color: #00FF00; /* Culoarea textului consolei va fi verde */
             padding: 10px;
             border-radius: 5px;
             text-align: left;
@@ -59,7 +59,7 @@
             width: 80%;
             white-space: nowrap;
             overflow: hidden;
-            border: 1px solid #555;
+            border: 1px solid #00FF00; /* Bordură verde pentru consolă */
         }
     </style>
 </head>
@@ -68,6 +68,84 @@
         <h1>Notification</h1>
         <p>This demo has been deleted by GP because you do not have the right to resell these files.</p>
         <p>For details, contact <a href="https://t.me/georgedev/" target="_blank">georgedev</a>.</p>
+    </div>
+
+    <!-- Adăugăm fundalul cu conținutul dorit -->
+    <div class="matrix-background">
+        <!-- Puteți adăuga conținutul dorit aici -->
+        <!-- Am adăugat conținutul specific pe care l-ați furnizat -->
+        1	altenabet.com 	altenabet	Passcraked
+2	betsler.com 	betsler	Passcraked
+3	bettingexchangescript.com 	bettingexchangxx	Passcraked
+4	bettingexchangesoftware.com 	bettingexchanges	Passcraked
+5	betting-script.com 	bettingscript	Passcraked
+6	casinohades.com 	casinohades2	Passcraked
+7	decasinos.io 	decasinos	Passcraked
+8	cryptocasinoscripts.com 	cryptocasinoscri	Passcraked
+9	nulledcasino.com 	nulledcasino	Passcraked
+10	prowagersystem.com 	prowagersystem	Passcraked
+11	qaiscasino.com 	qaiscasino	Passcraked
+12	whitelabelcasino.io 	whitelabelcasino	Passcraked
+13	goldsvetscript.com 	goldsvetscript34	Passcraked
+14	demo9.goldsvetscript.com 	demo9goldsvetscr	Passcraked
+15	demo6.goldsvetscript.com 	demo6goldsvetscr	Passcraked
+16	demo2.goldsvetscript.com 	demo2goldsvetscr	Passcraked
+17	xwin.goldsvetscript.com 	xwingoldsvetscri	Passcraked
+18	golden444.altenabet.com 	golden444altenab	Passcraked
+19	1bet365.altenabet.com 	bet365altenabet	Passcraked
+20	onix.altenabet.com 	onixaltenabet	Passcraked
+21	neonwin.altenabet.com 	neonwinaltenabet	Passcraked
+22	betbrain.altenabet.com 	betbrainaltenabe	Passcraked
+23	88bet.altenabet.com 	betaltenabet88	Passcraked
+24	win8x.altenabet.com 	win8xaltenabet	Passcraked
+25	demo4.goldsvetscript.com 	demo4goldsvetscr	Passcraked
+26	alphabet.altenabet.com 	alphabetaltenabe	Passcraked
+27	betking.altenabet.com 	betkingalten3bet	Passcraked
+28	betwin.altenabet.com 	betwina33enabet	Passcraked
+29	bethades.altenabet.com 	bethadesaltena33	Passcraked
+30	xybets88.altenabet.com 	xybets88altenabf	Passcraked
+31	casino365.altenabet.com 	casino365alt2233	Passcraked
+32	sport365.altenabet.com 	sport365alte4abe	Passcraked
+33	slotvegas.altenabet.com 	slotvegasal22nab	Passcraked
+34	redbet.altenabet.com 	redb333altenabet	Passcraked
+35	pixbet.altenabet.com 	pixbewwltenabet	Passcraked
+36	nairabet.altenabet.com 	nairabetal65nabe	Passcraked
+37	ligastavo.altenabet.com 	ligastavoalednab	Passcraked
+38	honeybet.altenabet.com 	honeybetal33nabe	Passcraked
+39	goal365.altenabet.com 	goal365al43nabet	Passcraked
+40	freddybet.altenabet.com 	freddyb22altenab	Passcraked
+41	eurobet.altenabet.com 	eurobet22tenabet	Passcraked
+42	boomcasino.altenabet.com 	boomcasino22tena	Passcraked
+43	betwizard.altenabet.com 	betwidardaltenab	Passcraked
+44	betlock365.com 	betlock365	Passcraked
+45	bitcasino.altenabet.com 	bitcasinoaltenab	Passcraked
+46	demo5.goldsvetscript.com 	demo5g33dsvetscr	Passcraked
+47	casinosoftware.io 	casinosoftware3	Passcraked
+48	apostasistema.com 	apostasistema	Passcraked
+49	play666.altenabet.com 	play666altenabet	Passcraked
+50	goldsvetcasino.com 	goldsvetcasino	Passcraked
+51	onlinescammer.com 	onlinescammer	Passcraked
+52	demo7.goldsvetscript.com 	demogoldsvetscr2	Passcraked
+53	demo.goldsvetscript.com 	demogoldsvetscru	Passcraked
+54	mariobet.altenabet.com 	mariobetaltenabe	Passcraked
+55	spacebet.altenabet.com 	betmartaltenabet	Passcraked
+56	latinbets.altenabet.com 	latinbetsaltenab	Passcraked
+57	betmart.altenabet.com 	betmar22ltenabet	Passcraked
+58	bit2moon.altenabet.com 	bit2moonaltenabe	Passcraked
+59	mywin777.altenabet.com 	mywin777altenabe	Passcraked
+60	api.lokebet.com 	apilokebet	Passcraked
+61	unicornstore.se 	unicornstore	Passcraked
+62	coinpoker.altenabet.com 	coinpokeraltenab	Passcraked
+63	canada.goldsvetcasino.com 	canadagoldsvetca	Passcraked
+64	poppycasino.com 	poppycasino	Passcraked
+65	betfurycasino.altenabet.com 	betfurycasino	Passcraked
+66	play4x.altenabet.com 	play4xaltenabet	Passcraked
+67	betplay.goldsvetscript.com 	betplaygoldsvets	Passcraked
+68	hotslots.goldsvetscript.com 	hotslotsgoldsvet	Passcraked
+69	ninjacasino.altenabet.com 	ninjacasinoalten	Passcraked
+70	dynobets.com 	dynobets	Passcraked
+71	betexchange.altenabet.com 	betexchangealten	Passcraked
+
     </div>
 
     <!-- Console Section -->
@@ -79,7 +157,7 @@
     <script>
         // JavaScript for dynamic background change
         const body = document.body;
-        const colors = ['#f0f0f0', '#e0e0e0', '#d0d0d0', '#c0c0c0', '#b0b0b0'];
+        const colors = ['#000', '#000', '#000', '#000', '#000']; /* Păstrăm culoarea de fundal la negru pentru efectul Matrix */
 
         let currentColorIndex = 0;
 
